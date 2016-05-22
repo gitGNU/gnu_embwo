c51091d5ae8242ab2ca558366fbd03c9428e21e0
gentooza <joa.cuellar@riseup.net>
2016-05-22 11:52:28 +0200

    GNU licensing guidelines 2

--- BWO.py
+++ BWO.py
@@ -1,19 +1,19 @@
 #! /usr/bin/env python
+#
+#    This file is part of 'Eleanor Margaret Burbidge Work Organizer' .
+#
+#    'Eleanor Margaret Burbidge Work Organizer' is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    'Eleanor Margaret Burbidge Work Organizer' is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-#
-#  Eleanor Margaret Burbidge Work Organizer  
-#
-#  Copyright 2016 by it's authors. 
-#
-#  Some rights reserved. See COPYING, AUTHORS.
-#  This file may be used under the terms of the GNU General Public
-#  License version 3.0 as published by the Free Software Foundation
-#  and appearing in the file COPYING included in the packaging of
-#  this file.
-#
-#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-#
 try:
    import os
    import sys
